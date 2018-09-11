43f1e0555aa936fc9b3096e201dbbcdba0a473bc
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-11 10:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(name, days){
-  return `It's ${days} days until ${holiday}!`
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`;
 }
